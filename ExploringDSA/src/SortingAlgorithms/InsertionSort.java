14c3daec0dbdf9791159696d873714fa3e60901b
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-06-01 17:28:03 +0530

    insertion sort implemented

--- ExploringDSA/src/SortingAlgorithms/InsertionSort.java
+++ ExploringDSA/src/SortingAlgorithms/InsertionSort.java
@@ -1,8 +1,23 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    //a lot of shifting
+    //can be improved with shell sort
     public static int[] insertionSort(int[] array){
-        
+        int i, newElement;
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++){
+            newElement = array[firstUnsortedIndex];
+            for(i = firstUnsortedIndex; i>0 && array[i-1] > newElement; i--){
+                array[i] = array[i-1];
+            }
+            array[i] = newElement;
+        }
         return array;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(InsertionSort.insertionSort(new int[] {23,6,69,78,15,36,45,29,4,62})));
     }
 }
---
+++ ExploringDSA/src/SortingAlgorithms/ShellSort.java
@@ -0,0 +1,9 @@
+package SortingAlgorithms;
+
+public class ShellSort {
+    //variation of insertion sort
+    public static int[] shellSort(int[] array){
+        
+        return array;
+    }
+}
