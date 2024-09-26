cadcc0587b3e8bfad55b7c760243b09242126568
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-09-26 20:09:59 +0530

    saving chnages

--- ExploringDSA/src/SortingAlgorithms/MergeSort.java
+++ ExploringDSA/src/SortingAlgorithms/MergeSort.java
@@ -1,4 +1,38 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class MergeSort {
+
+    //not a inplace algorithm
+    //stable algorithm
+    //memory extensive but fast
+    //O(nlogn) time complexity algorithm
+
+    public static void mergeSort(int[] input, int start , int end){
+        if(end - start< 2){
+            return;
+        }
+        int mid = (start + end )/2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid+1, end);
+        merge(input, start, mid, end);
+    }
+    public static void merge(int[] input, int start, int mid, int end){
+        int i = 0;
+        int j = mid+1;
+        int[] temp = new int[end - start];
+        int tempIndex = 0;
+        while(tempIndex < temp.length){
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{12,45,69,89,77,45,23,11,69,35};
+        MergeSort.mergeSort(arr, 0, arr.length);
+        System.out.println(Arrays.toString( arr));
+
+    }
 }
--- ExploringDSA/src/SortingAlgorithms/QuickSort.java
+++ ExploringDSA/src/SortingAlgorithms/QuickSort.java
@@ -1,4 +1,43 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class QuickSort {
+
+    public static void quickSort(int[] input, int start, int end){
+        if(end-start < 2){
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex-1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+        while(i < j){
+            while(i < j && input[--j]  >= pivot);
+            if(i<j){
+                input[i] = input [j];
+            }
+
+            while(i < j && input[++i] <= pivot);
+            if(i< j){
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{12,45,69,89,77,45,23,11,69,35};
+        QuickSort.quickSort(arr, 0, arr.length);
+        System.out.println(Arrays.toString( arr));
+
+    }
 }
